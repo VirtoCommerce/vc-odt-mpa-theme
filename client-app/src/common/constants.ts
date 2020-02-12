40c6e4f4ec63dab97df31d1808edefca765e472e
Aleksandr Vishnyakov <av@virtoway.com>
2020-02-12 22:41:48 +0500

    Fixed base url processing

--- client-app/src/common/constants.ts
+++ client-app/src/common/constants.ts
@@ -1,9 +1,9 @@
 //It is global variables initialized on the layout page
 export const storeName = window.STORE_NAME;
 export const locale = window.LOCALE;
-export const baseUrl = window.BASE_URL
-  .replace(/\/$/, "")
-  .replace(new RegExp(`/${storeName}$`), "")
-  .replace(new RegExp(`/${locale}$`), "");
+//Need to trim store and language from base URL because they will be added later as parameters for each API call.
+export const baseUrl = window.BASE_URL.replace(`/${storeName}`, "/")
+  .replace(`/${locale}`, "/")
+  .replace(/[/]+$/, "");
 
 // Other constants can be placed here
