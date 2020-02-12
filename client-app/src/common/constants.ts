c69035733066bb51b4427f224663c8f9e1008aeb
Eugeny Tatarincev <et@virtoway.com>
2020-02-12 17:26:16 +0200

    Fix base url paths

--- client-app/src/common/constants.ts
+++ client-app/src/common/constants.ts
@@ -1,6 +1,9 @@
 //It is global variables initialized on the layout page
-export const baseUrl = window.BASE_URL.replace(new RegExp("[/]+$"), "");
 export const storeName = (window as any).STORE_NAME;
-export const locale = (window as any).STORE_LNG;
+export const locale = (window as any).STORE_LANG;
+//Need to trim store and language from base URL because they will be added later as parameters for each API call. 
+export const baseUrl = window.BASE_URL.replace(`/${storeName}/`, "/")
+  .replace(`/${locale}/`, "/")
+  .replace(new RegExp("[/]+$"), "");
 
 // Other constants can be placed here
