41c27323280b9e97c100350fb1a607a195fca460
Aleksandr Vishniakov <av@virtoway.com>
2020-02-19 19:10:47 +0500

    VP-1509: Add message about no orders

--- client-app/src/pages/account/views/AccountInfo.vue
+++ client-app/src/pages/account/views/AccountInfo.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div class="mt-3">
     <loading :active.sync="isLoading"></loading>
 
     <fieldset>
       <div class="form-row form-group">
         <div class="col-md">
-          <label for="login">{{ $t("account.user_name") }}</label>
+          <label for="login">{{ $t("account.home.user_name") }}</label>
           <input id="login"
                  v-model="profile.userName"
                  type="text"
@@ -13,14 +13,14 @@
                  readonly>
         </div>
         <div class="col-md">
-          <label for="email">{{ $t("account.email") }}</label>
+          <label for="email">{{ $t("account.home.email") }}</label>
           <input id="email"
                  v-model="profile.email"
                  type="email"
                  class="form-control">
         </div>
         <div class="col-md">
-          <label for="phone">{{ $t("account.phone") }}</label>
+          <label for="phone">{{ $t("account.home.phone") }}</label>
           <input id="phone"
                  v-model="profile.phoneNumber"
                  type="text"
--- client-app/src/pages/account/views/AccountOrders.vue
+++ client-app/src/pages/account/views/AccountOrders.vue
@@ -2,39 +2,45 @@
   <div>
     <loading :active.sync="isLoading"></loading>
 
-    <b-table id="orders-list"
-             striped
-             hover
-             :items="ordersList.orders"
-             :fields="ordersList.listConfig.columns">
-      <!-- A custom formatted header cell for field 'name' -->
-      <template v-slot:head()="data">
-        <span class="text-info">{{ $t( `grids.orders_list.columns.${data.column.split('.').join('_')}`) }}</span>
-      </template>
+    <div v-if="ordersList.totalCount > 0">
+      <b-table id="orders-list"
+               striped
+               hover
+               :items="ordersList.orders"
+               :fields="ordersList.listConfig.columns">
+        <!-- A custom formatted header cell for field 'name' -->
+        <template v-slot:head()="data">
+          <span class="text-info">{{ $t( `account.orders.grid.columns.${data.column.split('.').join('_')}`) }}</span>
+        </template>
 
-      <!-- A custom formatted column -->
-      <template v-slot:cell(createdDate)="data">
-        <b class="text-info">{{ data.value | moment('ddd, DD/MM/YY') }}</b>, <b>{{ data.value.first }}</b>
-      </template>
-    </b-table>
+        <!-- A custom formatted column -->
+        <template v-slot:cell(createdDate)="data">
+          <b class="text-info">{{ data.value | moment('ddd, DD/MM/YY') }}</b>, <b>{{ data.value.first }}</b>
+        </template>
+      </b-table>
 
-    <div class="d-flex justify-content-between">
-      <b-pagination :value="ordersList.listConfig.pageNumber"
-                    aria-controls="oders-table"
-                    :total-rows="ordersList.totalCount"
-                    :per-page="ordersList.listConfig.pageSize"
-                    @change="pageChanged($event)"></b-pagination>
-      <div>
-        <select :value="ordersList.listConfig.pageSize"
-                class="form-control"
-                @change="pageSizeChanged($event.target.value)">
-          <option v-for="ps in pageSizes"
-                  :key="ps"
-                  :value="ps">
-            {{ ps }}
-          </option>
-        </select>
+      <div class="d-flex justify-content-between">
+        <b-pagination :value="ordersList.listConfig.pageNumber"
+                      aria-controls="oders-table"
+                      :total-rows="ordersList.totalCount"
+                      :per-page="ordersList.listConfig.pageSize"
+                      @change="pageChanged($event)"></b-pagination>
+        <div>
+          <select :value="ordersList.listConfig.pageSize"
+                  class="form-control"
+                  @change="pageSizeChanged($event.target.value)">
+            <option v-for="pageSize in pageSizes"
+                    :key="pageSize"
+                    :value="pageSize">
+              {{ pageSize }}
+            </option>
+          </select>
+        </div>
       </div>
+    </div>
+    <div v-if="!isLoading && ordersList.totalCount == 0"
+         class="mt-3">
+      <span>{{ $t("account.orders.no-orders") }}</span>
     </div>
   </div>
 </template>
