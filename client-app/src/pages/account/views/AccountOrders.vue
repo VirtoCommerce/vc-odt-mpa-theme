4a9391d9d5fa12e769b226d001c002f1ef7c5fd8
Ilya <ilya.ivanov6@gmail.com>
2020-02-28 20:25:18 +0200

    Remove highlighting after row clicking.

--- client-app/src/pages/account/views/AccountOrders.vue
+++ client-app/src/pages/account/views/AccountOrders.vue
@@ -1,39 +1,40 @@
 <template>
   <div>
     <loading :active.sync="isLoading" :z-index="5000"></loading>
-    <account-order-details-modal
-      :order-id="selectedOrderId">
+    <account-order-details-modal :order-id="selectedOrderId">
     </account-order-details-modal>
     <div v-if="ordersList.totalCount > 0">
-      <b-table id="orders-list"
-               stacked="md"
-               striped
-               hover
-               :items="ordersList.orders"
-               :fields="ordersList.listConfig.columns"
-               no-local-sorting
-               selectable
-               select-mode="single"
-               @sort-changed="sortChanged"
-               @row-selected="rowSelected">
+      <b-table
+        id="orders-list"
+        stacked="md"
+        striped
+        hover
+        :items="ordersList.orders"
+        :fields="ordersList.listConfig.columns"
+        no-local-sorting
+        @sort-changed="sortChanged">
         <template v-slot:cell(number)="data">
-          <a @click="openOrderDetails(data.value)">{{ data.value }}</a>
+          <a class="text-primary btn d-inline p-0" @click="openOrderDetails(data.value)">
+            {{ data.value }}
+          </a>
         </template>
         <template v-slot:cell(createdDate)="data">
-          <b class="text-info">{{ data.value | moment('ddd, DD/MM/YY') }}</b>
+          <span>{{ data.value | moment("ddd, DD/MM/YY") }}</span>
         </template>
       </b-table>
 
       <div class="d-flex justify-content-between">
-        <b-pagination :value="ordersList.listConfig.pageNumber"
-                      aria-controls="oders-table"
-                      :total-rows="ordersList.totalCount"
-                      :per-page="ordersList.listConfig.pageSize"
-                      @change="pageChanged($event)"></b-pagination>
+        <b-pagination
+          :value="ordersList.listConfig.pageNumber"
+          aria-controls="oders-table"
+          :total-rows="ordersList.totalCount"
+          :per-page="ordersList.listConfig.pageSize"
+          @change="pageChanged($event)"></b-pagination>
         <div>
-          <select :value="ordersList.listConfig.pageSize"
-                  class="form-control"
-                  @change="pageSizeChanged($event.target.value)">
+          <select
+            :value="ordersList.listConfig.pageSize"
+            class="form-control"
+            @change="pageSizeChanged($event.target.value)">
             <option v-for="pageSize in pageSizes"
                     :key="pageSize"
                     :value="pageSize">
@@ -43,45 +44,42 @@
         </div>
       </div>
     </div>
-    <div v-if="!isLoading && ordersList.totalCount == 0"
-         class="mt-3">
+    <div v-if="!isLoading && ordersList.totalCount == 0" class="mt-3">
       <span>{{ $t("account.orders.no-orders") }}</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-
 import Vue from "vue";
 import Component from "vue-class-component";
-import { namespace } from 'vuex-class';
+import { namespace } from "vuex-class";
 import { BvTableCtxObject } from "bootstrap-vue";
 import { FETCH_ORDERS, SET_ORDERS_LIST_CONFIG } from "@account/store/modules/orders-list/definitions";
-import { OrdersList, OrdersListConfig  } from "@account/store/modules/orders-list/types";
+import { OrdersList, OrdersListConfig } from "@account/store/modules/orders-list/types";
 import { CustomerOrder } from "@common/api/api-clients";
 import { pageSizes } from "@common/constants";
-import AccountOrderDetailsModal from './AccountOrderDetailsModal.vue';
+import AccountOrderDetailsModal from "./AccountOrderDetailsModal.vue";
 
-const ordersListModule = namespace('ordersListModule');
+const ordersListModule = namespace("ordersListModule");
 
 @Component({
   components: {
     AccountOrderDetailsModal
   }
 })
-export default class AccountOrders extends Vue{
-
-  @ordersListModule.Getter('ordersList')
+export default class AccountOrders extends Vue {
+  @ordersListModule.Getter("ordersList")
   private ordersList!: OrdersList;
 
-  @ordersListModule.Getter('isLoading')
+  @ordersListModule.Getter("isLoading")
   private isLoading!: boolean;
 
   @ordersListModule.Action(FETCH_ORDERS)
-  private fetchOrders!: () => OrdersList
+  private fetchOrders!: () => OrdersList;
 
   @ordersListModule.Action(SET_ORDERS_LIST_CONFIG)
-  private setListConfig!: (listConfig:  OrdersListConfig) => void
+  private setListConfig!: (listConfig: OrdersListConfig) => void;
 
   selectedOrder: CustomerOrder | null = null;
 
@@ -94,29 +92,24 @@ export default class AccountOrders extends Vue{
   }
 
   pageChanged(page: number) {
-    this.setListConfig(({ ...this.ordersList.listConfig, pageNumber: page }));
+    this.setListConfig({ ...this.ordersList.listConfig, pageNumber: page });
   }
 
   pageSizeChanged(pageSize: number) {
-    this.setListConfig(({ ...this.ordersList.listConfig, pageNumber: 1, pageSize: pageSize }));
+    this.setListConfig({ ...this.ordersList.listConfig, pageNumber: 1, pageSize: pageSize });
   }
 
   sortChanged(ctx: BvTableCtxObject) {
-    const sortDirection = ctx.sortDesc?"desc":"asc";
+    const sortDirection = ctx.sortDesc ? "desc" : "asc";
     const sortExpression = `${ctx.sortBy}:${sortDirection}`;
     const listConfig = { ...this.ordersList.listConfig, pageNumber: 1 };
-    listConfig.filters = {...this.ordersList.listConfig.filters, sort: sortExpression }
+    listConfig.filters = { ...this.ordersList.listConfig.filters, sort: sortExpression };
     this.setListConfig(listConfig);
-  }
-
-  rowSelected(items: CustomerOrder[]) {
-    this.selectedOrder = items[0];
   }
 
   openOrderDetails(orderId: string) {
     this.selectedOrderId = orderId;
-    this.$bvModal.show("orderDetailsModal")
+    this.$bvModal.show("orderDetailsModal");
   }
-
 }
 </script>
