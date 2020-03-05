050c1035b3a15f3bf671ab5e4283f10a51fa8391
Burashov Timofey <tburashov@gmail.com>
2020-03-05 15:43:47 +0700

    VP-1398,1399: Implement orders searching by status,keyword (#45)
    * VP-1398: Implement orders searching by status
    
    * VP-1399: Implement orders searching by keyword
    
    * Clean up code
    
    * Fix datepicker validation case
    
    * VP-1398 Rename uninformative names
    
    * VP-1398 rename allFiltersSelected and activeFilters
    
    Co-authored-by: Anton <superiorboroda@gmail.com>

--- client-app/src/pages/account/views/AccountOrders.vue
+++ client-app/src/pages/account/views/AccountOrders.vue
@@ -4,7 +4,7 @@
     <account-order-details-modal :order-id="selectedOrderId">
     </account-order-details-modal>
     <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-3">
         <label for="begin-date">{{ $t("account.orders.from") }}</label>
         <b-form-datepicker id="begin-date"
                            :value="ordersList.listConfig.filters.startDate"
@@ -18,7 +18,7 @@
                            class="mb-2"
                            @input="changeStartDate($event)"></b-form-datepicker>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-3">
         <label for="end-date">{{ $t("account.orders.to") }}</label>
         <b-form-datepicker id="end-date"
                            :value="ordersList.listConfig.filters.endDate"
@@ -31,6 +31,37 @@
                            v-bind="datepickerLabels"
                            class="mb-2"
                            @input="changeEndDate($event)"></b-form-datepicker>
+      </div>
+      <div class="col-md-3">
+        <label for="keyword-search">{{ $t("account.orders.keyword-search-title") }}</label>
+        <b-form-input id="keyword-search"
+                      type="text"
+                      :debounce="1000"
+                      :placeholder="$t('account.orders.enter-keyword')"
+                      :value="activeKeyword"
+                      @update="changeKeyword($event)"></b-form-input>
+      </div>
+      <div class="col-md-2 d-flex flex-column justify-content-center">
+        <label for="dropdown-filters">{{ $t("account.orders.status-filter.filter-by") }}</label>
+        <b-dropdown id="dropdown-filters"
+                    class="mb-2"
+                    variant="outline-primary"
+                    toggle-class="text-left"
+                    menu-class="p-2">
+          <template v-slot:button-content>
+            {{ allStatusesSelected || activeStatuses.length == 0 ? $t("account.orders.status-filter.all") : activeStatuses.length == 1 ? activeStatuses[0] : '...' }}
+          </template>
+          <b-form-checkbox
+            v-model="allStatusesSelected"
+            @change="toggleAllStatuses">
+            {{ allStatusesSelected ? $t("account.orders.status-filter.unselect-all") : $t("account.orders.status-filter.select-all") }}
+          </b-form-checkbox>
+          <b-form-checkbox-group
+            :checked="activeStatuses"
+            :options="availableOrderStatuses"
+            stacked
+            @change="selectedStatusesChanged($event)"></b-form-checkbox-group>
+        </b-dropdown>
       </div>
       <div class="col-md-12">
         <span v-if="!isDateValid && isDateValid != null" class="text-danger">{{ $t("account.orders.date-error") }}</span>
@@ -93,7 +124,7 @@ import { BvTableCtxObject } from "bootstrap-vue";
 import { FETCH_ORDERS, SET_ORDERS_LIST_CONFIG } from "@account/store/modules/orders-list/definitions";
 import { OrdersList, OrdersListConfig } from "@account/store/modules/orders-list/types";
 import { CustomerOrder } from "@common/api/api-clients";
-import { pageSizes, locale } from "@common/constants";
+import { pageSizes, locale, ordersStatuses } from "@common/constants";
 import AccountOrderDetailsModal from "./AccountOrderDetailsModal.vue";
 
 const ordersListModule = namespace("ordersListModule");
@@ -109,6 +140,12 @@ export default class AccountOrders extends Vue {
 
   @ordersListModule.Getter("isLoading")
   private isLoading!: boolean;
+
+  @ordersListModule.Getter("activeKeyword")
+  private activeKeyword!: string | undefined;
+
+  @ordersListModule.Getter("activeStatuses")
+  private activeStatuses!: string[];
 
   @ordersListModule.Action(FETCH_ORDERS)
   private fetchOrders!: () => OrdersList;
@@ -129,6 +166,10 @@ export default class AccountOrders extends Vue {
   datepickerLabels: LocaleMessages | {} = {};
 
   pageSizes = pageSizes;
+
+  availableOrderStatuses = ordersStatuses;
+
+  allStatusesSelected = true;
 
   locale = locale;
 
@@ -159,20 +200,20 @@ export default class AccountOrders extends Vue {
   }
 
   dateChanged() {
+    const listConfig = { ...this.ordersList.listConfig };
     if (this.startDate && this.endDate) {
       this.isDateValid = this.startDate <= this.endDate;
       if (this.isDateValid) {
-        this.setListConfig({ ...this.ordersList.listConfig, filters: {startDate: this.startDate, endDate: this.$moment(this.endDate).add(1, "days").subtract(1, "seconds").toDate()}})
+        listConfig.filters = { ...this.ordersList.listConfig.filters, startDate: this.startDate, endDate: this.prepareEndDate() };
+        this.setListConfig(listConfig);
       }
     }
     else  {
-      this.endDate === null && this.startDate === null ? this.isDateValid = null : this.isDateValid = true;
-      this.setListConfig({ ...this.ordersList.listConfig, filters: {startDate: this.startDate, endDate: this.endDate
-        ? this.$moment(this.endDate)
-          .add(1, "days")
-          .subtract(1, "seconds")
-          .toDate()
-        : undefined}})
+      this.endDate == null && this.startDate == null ? this.isDateValid = null : this.isDateValid = true;
+      listConfig.filters = { ...this.ordersList.listConfig.filters, startDate: this.startDate, endDate: this.endDate
+        ? this.prepareEndDate()
+        : undefined };
+      this.setListConfig(listConfig);
     }
   }
 
@@ -189,5 +230,29 @@ export default class AccountOrders extends Vue {
     this.endDate = date;
     this.dateChanged();
   }
+
+  changeKeyword(value: string) {
+    const listConfig = { ...this.ordersList.listConfig };
+    listConfig.filters = { ...this.ordersList.listConfig.filters, keyword: value };
+    this.setListConfig(listConfig);
+  }
+
+  selectedStatusesChanged(selectedStatuses: string[]) {
+    selectedStatuses.length != this.availableOrderStatuses.length ? this.allStatusesSelected = false : this.allStatusesSelected = true;
+    const listConfig = { ...this.ordersList.listConfig };
+    listConfig.filters = { ...this.ordersList.listConfig.filters, statuses: selectedStatuses };
+    this.setListConfig(listConfig);
+  }
+
+  toggleAllStatuses(selectedStatuses: string[]) {
+    const listConfig = { ...this.ordersList.listConfig };
+    selectedStatuses ? listConfig.filters = { ...this.ordersList.listConfig.filters, statuses: this.availableOrderStatuses } : listConfig.filters = { ...this.ordersList.listConfig.filters, statuses: [] };
+    this.setListConfig(listConfig);
+  }
+
+  private prepareEndDate(): Date {
+    return this.$moment(this.endDate).add(1, "days").subtract(1, "seconds").toDate();
+  }
+
 }
 </script>
