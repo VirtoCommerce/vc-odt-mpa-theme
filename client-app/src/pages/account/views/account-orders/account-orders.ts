d71aaeb93c06859171df7e0b1e337a4124dbb666
Burashov Timofey <tburashov@gmail.com>
2020-03-27 14:27:58 +0700

    fix wrong pipeline of opening the order details (#72)
    VP-1768

--- client-app/src/pages/account/views/account-orders/account-orders.ts
+++ client-app/src/pages/account/views/account-orders/account-orders.ts
@@ -3,7 +3,7 @@ import Component from "vue-class-component";
 import { namespace } from "vuex-class";
 import { BvTableCtxObject } from "bootstrap-vue";
 import OrderFilter from "@account/components/orders-filter/index.vue";
-import { FETCH_ORDERS, SET_ORDERS_LIST_CONFIG } from "@account/store/modules/orders-list/definitions";
+import { FETCH_ORDERS, SET_ORDERS_LIST_CONFIG , FETCH_ORDER } from "@account/store/modules/orders-list/definitions";
 import { OrdersList, OrdersListConfig, OrdersListFilters } from "@account/store/modules/orders-list/types";
 import { CustomerOrder } from "@common/api/api-clients";
 import { pageSizes, ordersStatuses } from "@common/constants";
@@ -30,9 +30,11 @@ export default class AccountOrders extends Vue {
   @ordersListModule.Action(SET_ORDERS_LIST_CONFIG)
   private setListConfig!: (listConfig: OrdersListConfig) => void;
 
-  selectedOrder: CustomerOrder | null = null;
+  @ordersListModule.Action(FETCH_ORDER)
+  private fetchOrder!: (orderId: string) => CustomerOrder;
 
-  selectedOrderId: string | null = null;
+  @ordersListModule.Getter("selectedOrder")
+  private selectedOrder!: CustomerOrder;
 
   pageSizes = pageSizes;
 
@@ -59,12 +61,11 @@ export default class AccountOrders extends Vue {
   }
 
   openOrderDetails(orderId: string) {
-    this.selectedOrderId = orderId;
+    this.fetchOrder(orderId);
     this.$bvModal.show("orderDetailsModal");
   }
 
   filtersChanged(filters: OrdersListFilters) {
     this.setListConfig({ ...this.ordersList.listConfig, filters });
   }
-
 }
