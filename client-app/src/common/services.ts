09232e544b78b0fcb3f3d8858265a30daf3c73bf
Timofey <tburashov@gmail.com>
2020-03-31 11:42:50 +0700

    VP-1781: add a class with constant service names

---
+++ client-app/src/common/services.ts
@@ -0,0 +1,21 @@
+export default class Services {
+  static OrderBrowsing = "ordersBrowsing";
+  static PaymentBrowsing = "paymentBrowsing";
+  static InvoiceBrowsing = "invoiceBrowsing";
+  static ManageUsers = "manageUsers";
+  static ManageRoles = "manageRoles";
+  static ContractsInfoBrowsing = "contractsInfoBrowsing";
+  static RetrieveReOrdering = "retrieveReOrdering";
+  static OrderApproval = "orderApproval";
+  static ProductTextSearch = "productTextSearch";
+  static ProductFiltering = "productFiltering";
+  static ProductDetailsBrowsing = "productDetailsBrowsing";
+  static ProductPriceBrowsing = "productPriceBrowsing";
+  static ProductRecommendation = "productRecommendation";
+  static OrderDraft = "orderDraft";
+  static WhishList = "whishList";
+  static SubmitOrder = "submitOrder";
+  static ProductInventoryBrowsing = "productInventoryBrowsing";
+  static ManageShipmentDetails = "manageShipmentDetails";
+  static ManagePaymentDetails = "managePaymentDetails";
+}
