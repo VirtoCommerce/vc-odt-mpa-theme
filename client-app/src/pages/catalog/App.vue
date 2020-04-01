5ec801c051db8723a39a89235111305ee5cfccdc
Aleksandr Vishniakov <av@virtoway.com>
2020-04-01 02:46:01 +0200

    Remove temporary tests

--- client-app/src/pages/catalog/App.vue
+++ client-app/src/pages/catalog/App.vue
@@ -7,22 +7,9 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import { ProductSearchCriteria } from "@common/models/search/product-search-criteria.ts";
-import { ProductSearchQuery } from "@common/models/search/product-search-query.ts";
-import { QueryBuilder } from "@common/services/query-builder.service.ts";
 
 @Component({
   name: "Catalog"
 })
-export default class App extends Vue {
-  get searchCriteria() {
-    const t1 = new URL(location.href).searchParams;
-    const t = new QueryBuilder(ProductSearchCriteria, ProductSearchQuery).fromQuery(t1);
-    //t.terms.toggle("Brand", "Prostejov Malting");
-    return t;
-  }
-  get searchQuery() {
-    return new QueryBuilder(ProductSearchCriteria, ProductSearchQuery).toQuery(this.searchCriteria).toString();
-  }
-}
+export default class App extends Vue {}
 </script>
