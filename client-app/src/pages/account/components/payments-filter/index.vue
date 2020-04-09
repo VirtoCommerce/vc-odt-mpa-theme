a5435bdf99b069c199b354ebbc034c34262857da
Aleksandr Vishniakov <av@virtoway.com>
2020-04-09 14:21:20 +0200

    Fixed bootstrap columns usage

--- client-app/src/pages/account/components/payments-filter/index.vue
+++ client-app/src/pages/account/components/payments-filter/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="searchCriteria">
-    <div class="d-flex flex-wrap flex-sm-row flex-column">
+    <div class="row flex-sm-row flex-column">
       <div class="col col-sm-6 col-md-5 col-lg-4">
         <label for="begin-date">{{ $t("account.payments.date-filter.from") }}</label>
         <b-form-datepicker
