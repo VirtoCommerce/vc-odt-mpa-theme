d9bdfbe751b9db218188f224d9ba9b901ba2f0f1
trueboroda <superiorboroda@gmail.com>
2020-04-09 13:27:06 +0300

    VP-1854: Implement the dummy component for change quantity of item
    Draft

--- client-app/src/components/shopping-cart/cart-item/cart-item.ts
+++ client-app/src/components/shopping-cart/cart-item/cart-item.ts
@@ -1,12 +1,16 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import { Prop, Emit } from "vue-property-decorator";
-import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
-import { CartLineItem } from '@common/api/api-clients';
+import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+import { CartLineItem } from "@common/api/api-clients";
+import ChangeItemQuantity from "@components/shopping-cart/change-item-quantity/index.vue";
 
 
 @Component({
-  name: "CartItem"
+  name: "CartItem",
+  components: {
+    ChangeItemQuantity
+  }
 })
 export default class CartItem extends Vue {
   @Prop()
---
+++ client-app/src/components/shopping-cart/change-item-quantity/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex jsutify-content-center">
+    <font-awesome-layers class="btn text-primary" @click="decrement()">
+      <font-awesome-icon :icon="minusIcon" size="lg"></font-awesome-icon>
+    </font-awesome-layers>
+    <input type="text"
+           OnlyNumber="true"
+           class="control"
+           :value="quantity"
+           @blur="textChanged($event.target.value)"
+           @keydown="onKeydown($event)">
+    <font-awesome-layers class="btn text-primary" @click="increment()">
+      <font-awesome-icon :icon="plusIcon" size="lg"></font-awesome-icon>
+    </font-awesome-layers>
+  </div>
+</template>
+
+<script lang="ts" src="./change-item-quantity.ts"></script>
