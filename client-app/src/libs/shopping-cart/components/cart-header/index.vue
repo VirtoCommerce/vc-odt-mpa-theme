59da06ea1fb075aea3ab867059b9606856ee7446
Ilya <ilya.ivanov6@gmail.com>
2020-04-15 10:46:53 +0200

    Fix long name case in editDraftHeader and width of input in Firefox

--- client-app/src/libs/shopping-cart/components/cart-header/index.vue
+++ client-app/src/libs/shopping-cart/components/cart-header/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="d-flex justify-content-beetween">
-    <h5 class="align-self-center m-0">
+  <div class="d-flex flex-wrap justify-content-between">
+    <h5 class="align-self-center m-0 text-break">
       {{ title }}
     </h5>
     <button v-if="cart.itemsCount != 0"
-            class="btn btn-link ml-auto px-0"
+            class="btn btn-link px-0"
             @click="onClearCartClicked()">
       {{ $t('shopping-cart.clear-cart') }}
     </button>
