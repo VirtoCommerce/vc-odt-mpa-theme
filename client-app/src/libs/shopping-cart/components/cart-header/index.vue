a1ec460551dbe3c15d0d5be07bdfb1fe43f88512
Ilya <ilya.ivanov6@gmail.com>
2020-04-15 12:48:30 +0200

    Fix remove button wrapping

--- client-app/src/libs/shopping-cart/components/cart-header/index.vue
+++ client-app/src/libs/shopping-cart/components/cart-header/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="d-flex flex-wrap justify-content-between">
+  <div class="d-flex justify-content-between">
     <h5 class="align-self-center m-0 text-break">
       {{ title }}
     </h5>
     <button v-if="cart.itemsCount != 0"
-            class="btn btn-link px-0"
+            class="btn btn-link px-0 text-nowrap"
             @click="onClearCartClicked()">
       {{ $t('shopping-cart.clear-cart') }}
     </button>
