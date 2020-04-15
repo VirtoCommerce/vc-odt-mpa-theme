cb3d5f77836b21fee63a9c41c3d93addc309460c
Ilya <ilya.ivanov6@gmail.com>
2020-04-15 19:36:59 +0200

    VP-1955:  Add duplicate check to add draft form
    + fix path for nswag output

--- client-app/src/libs/order-draft/components/add-draft-modal/add-draft-modal.ts
+++ client-app/src/libs/order-draft/components/add-draft-modal/add-draft-modal.ts
@@ -1,11 +1,27 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import { Validation } from "vuelidate"; //Don't remove this import
+import { storeName, locale, orderDraftType, startPageNumber, defaultPageSize } from "core/constants";
+import { listClient } from "core/services/api-clients.service";
 import { required } from "vuelidate/lib/validators";
+import { CartSearchCriteria } from '@core/api/api-clients';
 
 @Component({
   validations: {
-    draftName: { required }
+    draftName: {
+      required,
+      async isUnique(value) {
+        if (value === '') return true
+        const criteria = new CartSearchCriteria({type: orderDraftType, pageNumber: startPageNumber, pageSize: defaultPageSize});
+        criteria.keyword = value;
+        const result = await listClient.searchLists(criteria, storeName, locale);
+        if (result.totalCount == 0) {
+          return true
+        } else {
+          return false
+        }
+      }
+    }
   }
 })
 export default class AddDraftModal extends Vue {
@@ -22,4 +38,5 @@ export default class AddDraftModal extends Vue {
     this.$emit("draftAdded", this.draftName);
     this.$bvModal.hide("addDraftModal");
   }
+
 }
