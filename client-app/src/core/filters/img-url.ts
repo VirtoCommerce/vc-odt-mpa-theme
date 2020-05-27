b0e0e31abb4789246e96faf39c802c1f20f198e4
Anton <superiorboroda@gmail.com>
2020-05-27 14:10:40 +0300

    VP-2604: Implement vue directive to allow entering only number chars to the input element (#130)

--- client-app/src/core/filters/img-url.ts
+++ client-app/src/core/filters/img-url.ts
@@ -2,10 +2,10 @@ import Vue from "vue";
 import { appendToFilename } from "@core/utilities/utilities";
 
 
-export function appendSuffixToImgUrl(value: string, suffix: string) {
+Vue.filter('imgUrl', (value: string, suffix: string) => {
   if (!value) return '';
   if (!suffix) return value;
   const result = appendToFilename(value, `_${suffix}`);
   return result;
-}
+});
 
