3e87837c0627596bdd012b97d7a17895d486bb40
Anton <superiorboroda@gmail.com>
2020-05-29 05:23:02 +0300

    VP-2552: Improve imgUrl filter (#131)
    Check if suffix already exists

--- client-app/src/core/filters/img-url.ts
+++ client-app/src/core/filters/img-url.ts
@@ -5,7 +5,7 @@ import { appendToFilename } from "@core/utilities/utilities";
 Vue.filter('imgUrl', (value: string, suffix: string) => {
   if (!value) return '';
   if (!suffix) return value;
-  const result = appendToFilename(value, `_${suffix}`);
+  const result = appendToFilename(value, `_${suffix}`, true);
   return result;
 });
 
--- client-app/src/core/utilities/utilities.ts
+++ client-app/src/core/utilities/utilities.ts
@@ -16,8 +16,15 @@ export function toDictionary(obj: any): Dictionary<string | (string | null)[] | 
 }
 
 
-export function appendToFilename(filename: string, suffix: string){
+export function appendToFilename(filename: string, suffix: string, checkIfSuffixExists = false ){
   const dotIndex = filename.lastIndexOf(".");
-  if (dotIndex == -1) return filename + suffix;
-  else return filename.substring(0, dotIndex) + suffix + filename.substring(dotIndex);
+  let result = filename
+  if (dotIndex == -1) {
+    result = checkIfSuffixExists && filename.endsWith(suffix)?  filename : filename + suffix ;
+  }
+  else {
+    const fileNameWithoutExt = filename.substring(0, dotIndex);
+    result = checkIfSuffixExists && fileNameWithoutExt.endsWith(suffix) ? filename :  fileNameWithoutExt + suffix + filename.substring(dotIndex);
+  }
+  return result;
 }
