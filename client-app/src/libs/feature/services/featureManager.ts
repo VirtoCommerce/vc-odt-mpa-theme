e605306dbbbfb7a445e5ab0bd19b349737b72ef9
Timofey <tburashov@gmail.com>
2020-06-05 12:50:51 +0700

    VP-2360: fix the features toggling
    old the obtaining of states was replaced with new logics
    now the states will be obtained from constants

--- client-app/src/libs/feature/services/featureManager.ts
+++ client-app/src/libs/feature/services/featureManager.ts
@@ -1,27 +1,16 @@
-import json from '../../../../../config/settings_data.json';
-import { Feature } from '../models/feature';
+import { features } from 'core/constants';
 
 export function isActive(featureName: string) {
-  if (json == null) {
-    throw new Error("Couldn't obtain settings file.");
+  if (features == null) {
+    throw new Error("Couldn't obtain features.");
   }
 
-  if (json.features == null) {
-    throw new Error("Features section not specified in the settings file.")
+  const featureState = (features as any)[featureName];
+  if (featureState == null) {
+    throw new Error(`Couldn't obtain the feature by name "${featureName}"`);
   }
 
-  // eslint-disable-next-line
-  const untypedJson = json as any;
-
-  const feature = untypedJson.features[featureName] as Feature;
-  if (feature == null) {
-    return false;
-  }
-
-  // todo: here should be hard if-else logics
-  // todo: implement tests
-
-  return feature.IsActive;
+  return featureState;
 }
 
 
